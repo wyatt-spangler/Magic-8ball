e43d3b55ddd5724f26f49676835699d6f2de143c
Spangler_Wyatt <Spangler_Wyatt@edu.mcctc.net>
2020-09-15 09:43:35 -0400

    Add project files.

---
+++ Magic 8ball/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Magic_8ball
+{
+    class Program
+    {
+        static Random randomObject = new Random();
+        public static void Main(string[] args)
+        {
+            QuestionLoop();
+        }
+        static string GetQuestionFromUser()
+        {
+            Console.Write("What question do you have? ");
+            string questionString = Console.ReadLine();
+            return questionString;
+        }
+
+        static void QuestionLoop()
+        {
+            while(true)
+            {
+                string questionString = GetQuestionFromUser();
+                int randomNumber = randomObject.Next(4);
+                switch(randomNumber)
+                {
+                    case 0:
+                        {
+                            Console.WriteLine("Yes!");
+                            break;
+                        }
+                    case 1:
+                        {
+                            Console.WriteLine("No!");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Bad Idea!");
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("That's a mystery for the ages!");
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
